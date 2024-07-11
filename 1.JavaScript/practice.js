449acf10184591bd66e40c137d001f6de9117e8f
donghoonchoi <pitepan3@naver.com>
2024-07-11 18:37:33 +0900

    123

--- 1.JavaScript/practice.js
+++ 1.JavaScript/practice.js
@@ -1,11 +1,5 @@
-function makeFunc() {
-  const name = "Mozilla";
-  function displayName() {
-    console.log(name);
-  }
-  return displayName;
-}
-
-const myFunc = makeFunc();
-// myFunc();
-displayName();
+// 함수를 반환하는 함수
+const add = (x) => (y) => x + y;
+// 함수를 매개변수로 받는 함수
+const babies = ["dog", "cat", "hen"].map((name) => `baby ${name}`);
+add(1, 2)
