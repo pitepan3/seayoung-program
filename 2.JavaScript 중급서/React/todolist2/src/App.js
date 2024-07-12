211a4d8c8ef26efebc8746f6f9b29637064f6500
donghoonchoi <pitepan3@naver.com>
2024-07-12 23:47:11 +0900

    todolist2

--- 2.JavaScript 중급서/React/todolist2/src/App.js
+++ 2.JavaScript 중급서/React/todolist2/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+
+function TodoWightForm({newTodoTitle, setNewTodoTitle, addTodo}) {
+  return(
+    <div>
+      <input type="text" placeholder="새 할일을 입력해주세요"
+      value={newTodoTitle}
+      onChange={(e) => setNewTodoTitle(e.target.value)} />
+      &nbsp;
+      <button onClick={addTodo}>할 일 추가</button>
+    </div>
+  )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+  const[todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if(newTodoTitle.trim().length == 0) return; //trim() = 양옆 공백 제거
+
+    setTodos([...todos, newTodoTitle.trim()]);
+    setNewTodoTitle('');
+  }
+
+  return(
+    <div>
+      <TodoWightForm newTodoTitle={newTodoTitle} 
+      setNewTodoTitle={setNewTodoTitle} addTodo={addTodo}/>
+      
+      <hr />
+
+      <div>
+        {JSON.stringify(todos)}
+      </div>
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
