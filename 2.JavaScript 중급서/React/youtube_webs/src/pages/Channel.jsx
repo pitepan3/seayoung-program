857c1ebce1e4618fd1c58cbeedb4d195a954c66a
donghoonchoi <pitepan3@naver.com>
2024-07-25 16:04:29 +0900

    페이지 SEO작업

--- 2.JavaScript 중급서/React/youtube_webs/src/pages/Channel.jsx
+++ 2.JavaScript 중급서/React/youtube_webs/src/pages/Channel.jsx
@@ -3,7 +3,8 @@ import Main from '../components/section/Main'
 
 const Channel = () => {
   return (
-    <Main>
+    <Main title="유투브 채널" 
+      description="유투브 채널페이지입니다.">
       Channel
     </Main>
   )
